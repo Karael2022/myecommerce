2bc2b598a6cafbd99b608c7c49fc0bdc41aac880
Roxana Saucedo <kara.el2012@gmail.com>
2022-12-09 01:28:33 -0300

    Se agregaron fuentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Categories, Product, Products } from './screens';
 
+import { useFonts } from 'expo-font';
+
 export default function App() {
-  return <Categories />;
+const [loaded] = useFonts ({
+  'OpenSans-Regular':require('../assets/fonts/OpenSans-Regular.ttf'),
+  'OpenSans-Bold':require('../assets/fonts/OpenSans-Bold.ttf'),
+  'OpenSans-Italic':require('../assets/fonts/OpensSans-Italic.ttf'),
+});
+ return <Categories />;
 }
