137aa84cb13bc8a981ecc7dc0f51dc7a4bc24c80
Roxana Saucedo <kara.el2012@gmail.com>
2022-12-09 01:59:50 -0300

    Dispositivo nuevo funciona

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { Categories, Product, Products } from './screens';
 
 import { useFonts } from 'expo-font';
@@ -6,7 +7,25 @@ export default function App() {
 const [loaded] = useFonts ({
   'OpenSans-Regular':require('../assets/fonts/OpenSans-Regular.ttf'),
   'OpenSans-Bold':require('../assets/fonts/OpenSans-Bold.ttf'),
-  'OpenSans-Italic':require('../assets/fonts/OpensSans-Italic.ttf'),
+  'OpenSans-Italic':require('../assets/fonts/OpenSans-Italic.ttf'),
 });
+
+if (!loaded) {
+  return (
+    <View style = {styles.container}>
+      <ActivityIndicator color= '#c6e377'size='large' />
+      
+    </View>
+)}
+
+
  return <Categories />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
